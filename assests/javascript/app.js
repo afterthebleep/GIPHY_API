9db078e11159586d2a1cc0f47ee0082594f0a998
aishaortiz <developer.aishaortiz@gmail.com>
2019-09-09 22:26:34 -0700

    WIP GIF Functionality | Success: Rendered static images &  ratings data

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -17,6 +17,30 @@ function addButtons(searchArr, classToAdd, areaToAddTo) {
   }
 }
 
+$(document).on('click', '.searchButton', function () {
+  var type = $(this).data('type');
+  var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=wV6sQPcK7mV9puokHVI4XYKZq7w7Dc24&limit=10';
+  $.ajax({ url: queryURL, method: 'GET' })
+      .then(function (res) {
+          for(var i = 0; i < res.data.length; i++) {
+              var searchDiv = $('<div class="search-item">');
+              var rating = res.data[i].rating;
+              var p = $('<p>').text('Rating: '+rating);
+              var start = res.data[i].images.fixed_height.url;
+              var stop = res.data[i].images.fixed_height_still.url;
+              var img = $('<img>');
+              img.attr('src', stop);
+              img.attr('data-stop', stop);
+              img.attr('data-start', start);
+              img.attr('data-state', 'stop');
+              img.addClass('searchImage');
+              searchDiv.append(p);
+              searchDiv.append(img);
+              $('#searches').append(searchDiv);
+            }
+  });
+});
+
 
 
 
